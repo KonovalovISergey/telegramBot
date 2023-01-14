b2ecefab7f8debfe929ee9720987d1a2221f089b
KonovalovISergey <kon-s1998@mail.ru>
2023-01-14 20:04:06 +0300

    Init commit

---
+++ src/main/java/ru/tB/TBApplication.java
@@ -0,0 +1,13 @@
+package ru.tB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tB/TBApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
